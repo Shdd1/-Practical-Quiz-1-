b7c976e8ed028685e7cb26666571b92f1505fee5
Shdd1 <129505192+Shdd1@users.noreply.github.com>
2024-07-09 11:46:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,86 @@
+import com.sun.glass.ui.Size;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//*************Q1********************
+       /* Scanner sc=new Scanner(System.in);
+        System.out.println("inter number1 :");
+        int num1=sc.nextInt();
+        System.out.println("inter number2 :");
+        int num2=sc.nextInt();
+        System.out.println("inter number3 :");
+        int num3=sc.nextInt();
+        if(num1>num2&&num1>num3){
+            System.out.println("The largest number "+num1);
+        } else if (num2>num1&&num2>num3) {
+            System.out.println("The largest number "+num2);
+        }else {
+            System.out.println("The largest number "+num3);
+        }*/
+//************Q2**********************
+
+      /* Scanner sc=new Scanner(System.in);
+        System.out.println("inter string :");
+        String word=sc.nextLine();
+        System.out.println("inter number :");
+        int num=sc.nextInt();
+        for (int i=0;i<word.length();i++){
+            if(num==i){
+               System.out.println(word.charAt(num));
+            }
+        }*/
+
+//************Q3**********************
+        /*Scanner sc=new Scanner(System.in);
+        int num;
+        int sum=0;
+        while (true){
+            System.out.println("plese inter the number : if you want stope inter -1 ");
+             num=sc.nextInt();
+
+             if (num==-1){
+                 break;
+             }else{
+                 sum+=num;
+             }
+
+        }
+        System.out.println("sum= "+sum);*/
+
+
+//************Q4**********************
+/*int []array={10, -21 , 30, 31, -25};
+for (int i=0;i<array.length;i++){
+    if(array[i]>0){
+        System.out.println(array[i]+" is a positive number");
+    }
+    else {
+        System.out.println(array[i]+" is a negative number");
+    }
+}*/
+
+
+//************Q5*********************
+// ماعرفت احله شكرا على السؤال عرفت وين نقطة ضعفي
+      /*  ArrayList<String>ar=new ArrayList<>();
+
+        ar.add("Tuwaiq");
+        ar.add("Bootcamp");
+        ar.add("Student");
+        ar.add("JAVA");
+        int count=0;
+        for (String s:ar){
+            if(s.length()<5){
+                System.out.println(s);
+
+            }
+        }*/
+
+
+    }
+}
